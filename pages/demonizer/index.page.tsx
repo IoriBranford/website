e917299c7621dc1085d67ae3b368fa56a68e1ec4
IoriBranford <ioribranford@gmail.com>
2023-10-10 16:29:59 -0700

    Add section styles for game pages.

--- pages/demonizer/index.page.tsx
+++ pages/demonizer/index.page.tsx
@@ -1,12 +1,13 @@
 import About from "./about.mdx";
+import './style.css'
 
 export function Page() {
   return (
-    <>
+    <section>
       <img src="/demonizer/cover-art.png" />
       <div style={{ textAlign: "left" }}>
         <About />
       </div>
-    </>
+    </section>
   );
 }
--- pages/honey-soldier/index.page.tsx
+++ pages/honey-soldier/index.page.tsx
@@ -1,12 +1,13 @@
 import About from "./about.mdx";
+import './style.css'
 
 export function Page() {
   return (
-    <>
+    <section>
       <img src="/honey-soldier/cover-art.jpg" />
       <div style={{ textAlign: "left" }}>
         <About />
       </div>
-    </>
+    </section>
   );
 }
