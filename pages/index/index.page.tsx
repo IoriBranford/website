83dd0e5608ed9f182bc996921593d611f95370a2
IoriBranford <ioribranford@gmail.com>
2023-10-10 15:14:01 -0700

    Add avatar, name, and games link to main page.

--- pages/index/index.page.tsx
+++ pages/index/index.page.tsx
@@ -1,28 +1,15 @@
-import preactLogo from '../assets/preact.svg';
 import './style.css';
 
 export function Page() {
 	return (
 		<div>
-			<a href="https://preactjs.com" target="_blank">
-				<img src={preactLogo} alt="Preact logo" height="160" width="160" />
-			</a>
-			<h1>Get Started building Vite-powered Preact Apps </h1>
+			<img src='/avatar.jpg' alt="Iori Branford's avatar" height="160" width="160" />
+			<h1>Iori Branford</h1>
 			<section>
 				<Resource
-					title="Learn Preact"
-					description="If you're new to Preact, try the interactive tutorial to learn important concepts"
-					href="https://preactjs.com/tutorial"
-				/>
-				<Resource
-					title="Differences to React"
-					description="If you're coming from React, you may want to check out our docs to see where Preact differs"
-					href="https://preactjs.com/guide/v10/differences-to-react"
-				/>
-				<Resource
-					title="Learn Vite"
-					description="To learn more about Vite and how you can customize it to fit your needs, take a look at their excellent documentation"
-					href="https://vitejs.dev"
+					title="Games"
+					description="Games I've made"
+					href="/games"
 				/>
 			</section>
 		</div>
@@ -31,7 +18,7 @@ export function Page() {
 
 function Resource(props) {
 	return (
-		<a href={props.href} target="_blank" class="resource">
+		<a href={props.href} class="resource">
 			<h2>{props.title}</h2>
 			<p>{props.description}</p>
 		</a>
