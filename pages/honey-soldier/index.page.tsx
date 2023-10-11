ef92003cf6e5977eaa4e3d0c2bc62ef35ce5701f
IoriBranford <ioribranford@gmail.com>
2023-10-11 15:47:28 -0700

    List platforms available at each store.

--- pages/honey-soldier/index.page.tsx
+++ pages/honey-soldier/index.page.tsx
@@ -59,26 +59,32 @@ export function Page() {
         <article style={{ textAlign: "left" }}>
           <About />
           <h2 style={{ textAlign: "center" }}>Play for Free</h2>
-          <section>
-            <iframe
-              frameborder="0"
-              src="https://itch.io/embed/1579324?bg_color=f1e49d&amp;fg_color=000000&amp;link_color=fa5c5c&amp;border_color=a17c18"
-              width="208"
-              height="167"
-              title="Honey Guardian on itch.io"
-            >
-              <a href="https://ioribranford.itch.io/honey-guardian">
-                Honey Guardian by IoriBranford
+          <section style={{ textAlign: "center" }}>
+            <div>
+              <iframe
+                frameborder="0"
+                src="https://itch.io/embed/1579324?bg_color=f1e49d&amp;fg_color=000000&amp;link_color=fa5c5c&amp;border_color=a17c18"
+                width="208"
+                height="167"
+                title="Honey Guardian on itch.io"
+              >
+                <a href="https://ioribranford.itch.io/honey-guardian">
+                  Honey Guardian by IoriBranford
+                </a>
+              </iframe>
+              <p>For Windows, LÖVE, Web, Android</p>
+            </div>
+            <div>
+              <a href="https://play.google.com/store/apps/details?id=com.ioribranford.honeysoldier&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
+                <img
+                  width="258"
+                  height="100"
+                  alt="Get it on Google Play"
+                  src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
+                />
               </a>
-            </iframe>
-            <a href="https://play.google.com/store/apps/details?id=com.ioribranford.honeysoldier&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
-              <img
-                width="258"
-                height="100"
-                alt="Get it on Google Play"
-                src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
-              />
-            </a>
+              <p>For Android</p>
+            </div>
           </section>
         </article>
       </section>
