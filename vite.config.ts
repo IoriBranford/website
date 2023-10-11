969b087715a1877e3240407e6dc79e1f1869e2bf
IoriBranford <ioribranford@gmail.com>
2023-10-10 17:36:40 -0700

    Use vite-plugin-mkcert to test locally with https.

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,17 @@ import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 import ssr from 'vike/plugin';
 import mdx from '@mdx-js/rollup';
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	server: {
+		https: true
+	},
 	plugins: [
 		preact(),
 		ssr({ prerender: true }),
-		mdx()
+		mdx(),
+		mkcert()
 	],
 });
