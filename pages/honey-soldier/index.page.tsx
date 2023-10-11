d253065dc1cf2a37abf1bb75ba7397eda21e05d3
IoriBranford <ioribranford@gmail.com>
2023-10-10 19:19:10 -0700

    Move Honey Soldier play links to a row under the description.

--- pages/honey-soldier/index.page.tsx
+++ pages/honey-soldier/index.page.tsx
@@ -14,6 +14,11 @@ export function Page() {
         <img src="/honey-soldier/cover-art.jpg" alt='Honey Soldier key art' width={756} height={600}/>
         <div style={{ textAlign: "left" }}>
           <About />
+          <h2 style={{ textAlign: "center" }}>Play for Free</h2>
+          <section>
+            <iframe frameborder="0" src="https://itch.io/embed/1579324?bg_color=f1e49d&amp;fg_color=000000&amp;link_color=fa5c5c&amp;border_color=a17c18" width="208" height="167"><a href="https://ioribranford.itch.io/honey-guardian">Honey Guardian by IoriBranford</a></iframe>
+            <a href='https://play.google.com/store/apps/details?id=com.ioribranford.honeysoldier&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img width='258' height='100' alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
+          </section>
         </div>
       </section>
     </div>
