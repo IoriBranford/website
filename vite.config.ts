39f58930f424474e7903bf2d6093ed9893a8b368
IoriBranford <ioribranford@gmail.com>
2023-10-12 15:35:53 -0700

    Install vite-imagetools. Define params for a gallery image.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import preact from '@preact/preset-vite';
 import ssr from 'vike/plugin';
 import mdx from '@mdx-js/rollup';
 import mkcert from 'vite-plugin-mkcert'
+import {imagetools} from 'vite-imagetools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,6 +22,18 @@ export default defineConfig({
 		preact(),
 		ssr({ prerender: true }),
 		mdx({ jsxImportSource: 'preact' }),
-		mkcert()
+		mkcert(),
+		imagetools({
+			defaultDirectives: (url) => {
+				const params0 = url.searchParams
+				let params = "?format=webp"
+				if (params0.has("galleryimg")) {
+					params = "?as=srcset&format=webp&h=240;480"
+				} else {
+					params = "?format=webp"
+				}
+				return new URLSearchParams(params)
+			}
+		})
 	],
 });
