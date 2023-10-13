ef206dd2d7125369cfb611d24a49036ff07d533c
IoriBranford <ioribranford@gmail.com>
2023-10-12 19:56:44 -0700

    Add Demonizer gallery and make logo responsive.

--- pages/demonizer/index.page.tsx
+++ pages/demonizer/index.page.tsx
@@ -1,20 +1,52 @@
 import About from "./about.mdx";
 import "../gamepage.css";
+import Gallery, {
+  GalleryIFrame,
+  GalleryImage,
+  GalleryItem,
+} from "../../components/Gallery";
+
+import Logo from "./assets/logo.png?logo";
+import CoverArt from "./assets/cover-art.png?galleryimg";
 
 export const documentProps = {
-	title: 'Demonizer',
-	description: "The fantasy bullet battle to end monstergirl genocide!"
-}
+  title: "Demonizer",
+  description: "The fantasy bullet battle to end monstergirl genocide!",
+};
+
+const GalleryItems: GalleryItem[] = [
+  {
+    element: <GalleryImage srcset={CoverArt} alt="Honey Soldier key art" />,
+    thumbnail: CoverArt,
+    caption: "",
+  },
+  {
+    element: (
+      <GalleryIFrame
+        src="https://www.youtube.com/embed/TGYRJMf5UbM?si=6wN1NIq0GRNjcPes"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      />
+    ),
+    thumbnail: "https://img.youtube.com/vi/TGYRJMf5UbM/mqdefault.jpg",
+    caption: "",
+  },
+];
 
 export function Page() {
   return (
     <div>
-      <img src="/demonizer/logo.png" alt='Demonizer logo' width={960} height={240} />
+      <img
+        class="logo"
+        srcset={Logo}
+        alt="Demonizer logo"
+        sizes="(max-width: 600px) 640px, 1280px"
+      />
       <section>
-        <img src="/demonizer/cover-art.png" alt='Demonizer key art' width={696} height={900} />
-        <div style={{ textAlign: "left" }}>
+        <Gallery items={GalleryItems} />
+        <article style={{ textAlign: "left" }}>
           <About />
-        </div>
+        </article>
       </section>
     </div>
   );
