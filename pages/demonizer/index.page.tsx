c66dfa898b4873dc3599f1349617e6bfcbf8befe
IoriBranford <ioribranford@gmail.com>
2023-10-16 16:37:21 -0700

    Move game page contents to MDX files so Markdown can be used directly in them.

--- pages/demonizer/index.page.tsx
+++ pages/demonizer/index.page.tsx
@@ -1,80 +1,10 @@
-import About from "./about.mdx";
-import "../gamepage.css";
-import Gallery, {
-  GalleryImage,
-  GalleryItem,
-} from "../../components/Gallery";
-
-import Logo from "./assets/logo.png?logo";
-import LinkButtons, { StoreLinkButton } from "#root/components/LinkButtons";
-
-const ImageItems : Record<string, string> = import.meta.glob([
-  './gallery/*.png', './gallery/*.jpg',
-], {
-  eager: true,
-  import: 'default',
-  query: {
-    galleryimg: true
-  }
-})
-
-const OriginalImages : Record<string, string> = import.meta.glob([
-  './gallery/*.png', './gallery/*.jpg',
-], {
-  eager: true,
-  import: 'default',
-})
-
-const GalleryComponentItems : Record<string, GalleryItem> = import.meta.glob([
-  './gallery/*.tsx'
-], {
-  eager: true,
-  import: 'default'
-})
+import Content from './Content.mdx'
 
 export const documentProps = {
   title: "Demonizer",
   description: "The fantasy bullet battle to end monstergirl genocide!",
 };
 
-const GalleryItems: GalleryItem[] = [
-  ...Object.entries(ImageItems).map((item) => {return {
-    element: <GalleryImage src={OriginalImages[item[0]]} srcset={item[1]} alt={item[0]}/>,
-    thumbnail: item[1],
-    caption: ""
-  }}),
-  ...Object.values(GalleryComponentItems)
-]
-
 export function Page() {
-  return (
-    <div>
-      <img
-        class="logo"
-        srcset={Logo}
-        alt="Demonizer logo"
-        width="640"
-        height="160"
-      />
-      <section>
-        <Gallery items={GalleryItems} />
-        <article style={{ textAlign: "left" }}>
-          <About />
-          <h2 style={{ textAlign: "center" }}>Buy</h2>
-          <LinkButtons>
-            <StoreLinkButton
-              store="itch.io"
-              oses={["windows", "linux", "apple", "android",]}
-              href="https://ioribranford.itch.io/demonizer"
-            />
-            <StoreLinkButton
-              store="Steam"
-              oses={["windows", "linux"]}
-              href="https://store.steampowered.com/app/1091390/Demonizer"
-            />
-          </LinkButtons>
-        </article>
-      </section>
-    </div>
-  );
+  return <Content/>
 }
--- pages/honey-soldier/index.page.tsx
+++ pages/honey-soldier/index.page.tsx
@@ -1,71 +1,10 @@
-import About from "./about.mdx";
-import "../gamepage.css";
-import Gallery, { GalleryImage, GalleryItem } from "../../components/Gallery";
-import LinkButtons, {
-  StoreLinkButton,
-} from "#root/components/LinkButtons";
-
-const ImageItems: Record<string, string> = import.meta.glob(
-  ["./gallery/*.png", "./gallery/*.jpg"],
-  {
-    eager: true,
-    import: "default",
-    query: {
-      galleryimg: true,
-    },
-  }
-);
-
-const OriginalImages: Record<string, string> = import.meta.glob(
-  ["./gallery/*.png", "./gallery/*.jpg"],
-  {
-    eager: true,
-    import: "default",
-  }
-);
+import Content from './Content.mdx'
 
 export const documentProps = {
   title: "Honey Soldier",
   description: "Shoot invaders out of your hive in two minutes",
 };
 
-const GalleryItems: GalleryItem[] = Object.entries(ImageItems).map((item) => {
-  return {
-    element: (
-      <GalleryImage
-        src={OriginalImages[item[0]]}
-        srcset={item[1]}
-        alt={item[0]}
-      />
-    ),
-    thumbnail: item[1],
-    caption: "",
-  };
-});
-
 export function Page() {
-  return (
-    <div>
-      <h1>Honey Soldier</h1>
-      <section>
-        <Gallery items={GalleryItems} />
-        <article style={{ textAlign: "left" }}>
-          <About />
-          <h2 style={{ textAlign: "center" }}>Play for Free</h2>
-          <LinkButtons>
-            <StoreLinkButton
-              store="itch.io"
-              oses={["windows", "linux", "apple", "android", "html5"]}
-              href="https://ioribranford.itch.io/honey-guardian"
-            />
-            <StoreLinkButton
-              store="Google Play"
-              oses={["android"]}
-              href="https://play.google.com/store/apps/details?id=com.ioribranford.honeysoldier"
-            />
-          </LinkButtons>
-        </article>
-      </section>
-    </div>
-  );
+  return <Content/>
 }
