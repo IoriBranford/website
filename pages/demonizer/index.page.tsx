3970f4406837c57d369d93816bc6c7c19d4923c4
IoriBranford <ioribranford@gmail.com>
2023-10-15 20:05:34 -0700

    Add Demonizer store link buttons.

--- pages/demonizer/index.page.tsx
+++ pages/demonizer/index.page.tsx
@@ -6,6 +6,7 @@ import Gallery, {
 } from "../../components/Gallery";
 
 import Logo from "./assets/logo.png?logo";
+import LinkButtons, { StoreLinkButton } from "#root/components/LinkButtons";
 
 const ImageItems : Record<string, string> = import.meta.glob([
   './gallery/*.png', './gallery/*.jpg',
@@ -59,6 +60,19 @@ export function Page() {
         <Gallery items={GalleryItems} />
         <article style={{ textAlign: "left" }}>
           <About />
+          <h2 style={{ textAlign: "center" }}>Buy</h2>
+          <LinkButtons>
+            <StoreLinkButton
+              store="itch.io"
+              oses={["windows", "linux", "apple", "android",]}
+              href="https://ioribranford.itch.io/demonizer"
+            />
+            <StoreLinkButton
+              store="Steam"
+              oses={["windows", "linux"]}
+              href="https://store.steampowered.com/app/1091390/Demonizer"
+            />
+          </LinkButtons>
         </article>
       </section>
     </div>
