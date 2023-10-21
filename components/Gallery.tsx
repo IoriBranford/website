c01981f6fa89834585a9d8bb43eacdb335564438
IoriBranford <ioribranford@gmail.com>
2023-10-20 20:35:30 -0700

    Full-view images in same window.

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,12 +1,23 @@
-import { useState } from "preact/hooks";
+import { useContext, useState } from "preact/hooks";
 import { JSX } from "preact/jsx-runtime";
 import "./Gallery.css";
+import { createContext } from "preact";
+
+const GalleryContext = createContext({
+  setFullView: (_:boolean)=>{}
+});
 
 export function GalleryImage({ src = "", srcset = "", alt = "" }) {
+  const { setFullView } = useContext(GalleryContext);
   return (
-    <a href={src} target='__blank'>
-      <img class="active" sizes="640px" src={src} srcset={srcset} alt={alt} />
-    </a>
+    <img
+      class="active"
+      sizes="(max-width: 640px) 640px, 1280px"
+      src={src}
+      srcset={srcset}
+      alt={alt}
+      onClick={() => setFullView(true)}
+    />
   );
 }
 
@@ -26,20 +37,30 @@ export interface GalleryItem {
 
 export default function Gallery({ items }: { items: GalleryItem[] }) {
   const [activeIndex, setActiveIndex] = useState<number>(0);
+  const [fullView, setFullView] = useState<boolean>(false);
   return (
-    <div class="gallery">
-      {items[activeIndex].element}
-      <section class="thumbnailgrid">
-        {items.map((item, itemi) => (
-          <img
-            class={itemi == activeIndex ? "thumbnail-selected" : "thumbnail"}
-            onClick={() => setActiveIndex(itemi)}
-            sizes="160px"
-            srcset={item.thumbnail}
-            alt={item.caption}
-          />
-        ))}
-      </section>
-    </div>
+    <GalleryContext.Provider value={{ setFullView }}>
+      <div class="gallery">
+        {items[activeIndex].element}
+        <section class="thumbnailgrid">
+          {items.map((item, itemi) => (
+            <img
+              class={itemi == activeIndex ? "thumbnail-selected" : "thumbnail"}
+              onClick={() => setActiveIndex(itemi)}
+              sizes="160px"
+              srcset={item.thumbnail}
+              alt={item.caption}
+            />
+          ))}
+        </section>
+        {fullView ? (
+          <div class="fullview" onClick={() => setFullView(false)}>
+            {items[activeIndex].element}
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    </GalleryContext.Provider>
   );
 }
