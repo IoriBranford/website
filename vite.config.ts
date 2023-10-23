e115be7b9c26da231624769c203dad5484e0aa73
IoriBranford <ioribranford@gmail.com>
2023-10-23 12:12:40 -0700

    Try removing preact aliases which may be redundant.

--- vite.config.ts
+++ vite.config.ts
@@ -14,10 +14,6 @@ export default defineConfig({
 	},
 	resolve: {
 		alias: [
-			{ find: 'react', replacement: 'preact/compat' },
-			{ find: 'react-dom/test-utils', replacement: 'preact/test-utils' },
-			{ find: 'react-dom', replacement: 'preact/compat' },
-			{ find: 'react/jsx-runtime', replacement: 'preact/jsx-runtime' },
 			{ find: '#root', replacement: __dirname }
 		],
 	},
