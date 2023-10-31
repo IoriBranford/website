b3413aae151ea88df8eeec6c3430d481b7164517
IoriBranford <ioribranford@gmail.com>
2023-10-31 00:12:14 -0700

    Prep Demonizer bonus gallery for adding item info.

---
+++ pages/demonizer/bonus/commissions/_items.ts
@@ -0,0 +1,32 @@
+import { GalleryItem, GalleryItems } from "#root/components/Gallery";
+
+const ImageItems: Record<string, string> = import.meta.glob(
+  ["./*.png", "./*.jpg"],
+  {
+    eager: true,
+    import: "default",
+    query: {
+      galleryimg: true,
+    },
+  }
+);
+
+const OriginalImages: Record<string, string> = import.meta.glob(
+  ["./*.png", "./*.jpg"],
+  {
+    eager: true,
+    import: "default",
+  }
+);
+
+const ElementItems: Record<string, GalleryItem> = import.meta.glob(
+  ["./*.tsx"],
+  {
+    eager: true,
+    import: "default",
+  }
+);
+
+import ItemsCsv from './_items.csv?raw'
+
+export default GalleryItems(ImageItems, OriginalImages, ElementItems, ItemsCsv)
