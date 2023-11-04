277fb626ec11acbf2366dd1070e437bb4aae6e45
IoriBranford <ioribranford@gmail.com>
2023-11-03 23:24:22 -0700

    Add card images to games list. Use more space for games instead of name and avatar.

--- pages/index/index.page.server.tsx
+++ pages/index/index.page.server.tsx
@@ -8,38 +8,35 @@ export const documentProps = {
 }
 
 export function Page() {
-	return (<>
-		<div class='grid' style={{placeItems: 'center'}}>
-			<div>
-				<img class='avatar' src='/avatar.png' alt="Iori Branford's avatar" height="256" width="256" />
-				<h1 style={{textAlign: 'center'}}>Iori Branford</h1>
-			</div>
-			<div>
-				<section>
-					<div class='grid'>
-						{Object.entries(AllGameProps).map(([id, game]) => (
-							<Resource href={`/${id}`} {...game}/>
-						))}
-					</div>
-				</section>
-				<section>
-					<div class='grid' style={{textAlign: 'center'}}>
-						<section><a href="/links"><b>🔗 Links</b></a></section>
-						<section><a href="/connect"><b>💬 Connect</b></a></section>
-						<section><a href="/donate"><b>💸 Donate</b></a></section>
-					</div>
-				</section>
+	return (
+		<div style={{justifyItems: 'center'}}>
+			<h1 style={{textAlign: 'center'}}><img class='avatar' src='/avatar.png' alt="Iori Branford's avatar" height="64" width="64" /> Iori Branford</h1>
+			<h2 style={{textAlign: 'center'}}>Games</h2>
+			<section>
+				<div class='grid'>
+					{Object.entries(AllGameProps).map(([id, game]) => (
+						<Resource href={`/${id}`} {...game}/>
+					))}
+				</div>
+			</section>
+			<div class='grid' style={{textAlign: 'center'}}>
+				<section><a href="/links"><b>🔗 Links</b></a></section>
+				<section><a href="/connect"><b>💬 Connect</b></a></section>
+				<section><a href="/donate"><b>💸 Donate</b></a></section>
 			</div>
 		</div>
-	</>);
+	);
 }
 
 function Resource(props) {
-	const {href, title, description} = props
-	return (
+	const {href, title, description, cardImg} = props
+	return (<section>
 		<a href={href}>
-			<b>{title}</b>
-			<p>{description}</p>
+			<button style={{position: 'relative', padding: 0}}>
+				<img src={cardImg} style={{borderRadius: 'var(--border-radius)'}}/>
+			</button>
 		</a>
-	);
+		<b>{title}</b>
+		<p>{description}</p>
+	</section>);
 }
