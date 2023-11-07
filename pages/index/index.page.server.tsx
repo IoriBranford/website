7490f74a7d7df2a58abdbf7aa520c92fed8171cc
IoriBranford <ioribranford@gmail.com>
2023-11-07 14:43:22 -0800

    Fit game buttons to card images and center justify them.

--- pages/index/index.page.server.tsx
+++ pages/index/index.page.server.tsx
@@ -13,7 +13,7 @@ export function Page() {
 			<h1 style={{textAlign: 'center'}}><img class='avatar' src='/avatar.png' alt="Iori Branford's avatar" height="64" width="64" /> Iori Branford</h1>
 			<h2 style={{textAlign: 'center'}}>Games</h2>
 			<section>
-				<div class='grid'>
+				<div class='grid' style={{justifyItems: 'center'}}>
 					{Object.entries(AllGameProps).map(([id, game]) => (
 						<Resource href={`/${id}`} {...game}/>
 					))}
@@ -32,7 +32,7 @@ function Resource(props) {
 	const {href, title, description, image} = props
 	return (<section>
 		<a href={href}>
-			<button style={{position: 'relative', padding: 0}} title={title}>
+			<button style={{width: 'fit-content', padding: 0}} title={title}>
 				<img src={image} alt={title} style={{borderRadius: 'var(--border-radius)'}}/>
 			</button>
 			<b>{title}</b>
