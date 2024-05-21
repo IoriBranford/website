ead5cab5fb942a68c5655d8be99bebd67d897177
IoriBranford <ioribranford@gmail.com>
2024-05-21 00:52:43 -0700

    Support HTML-only render mode.

--- renderer/+config.ts
+++ renderer/+config.ts
@@ -1,9 +1,26 @@
-import type { Config } from 'vike/types'
+import type { Config, ConfigEnv } from 'vike/types'
 
 export default {
     passToClient: ['pageProps', 'urlPathname'],
-
+    
     // To enable Client-side Routing:
     // clientRouting: true,
     // !! WARNING !! Before doing so, read https://vike.dev/clientRouting
+    
+    meta: {
+        renderMode: {
+            env: { config: true },
+            effect({ configDefinedAt, configValue }) {
+                let env: ConfigEnv = {
+                    server: true,
+                    client: configValue != 'HTML',
+                }
+                return {
+                    meta: {
+                        Page: { env }
+                    }
+                }
+            }
+        }
+    },
 } satisfies Config
