59d868b5aa4946387266fde284c8a6a08ca0d02b
IoriBranford <ioribranford@gmail.com>
2024-08-10 12:33:31 -0700

    Add Honey Soldier MD development article.

---
+++ pages/honeysoldier/genesisdev/+config.ts
@@ -0,0 +1,6 @@
+export default {
+    documentProps: {
+        "title": "Honey Soldier - Genesis/Megadrive Development",
+        "description": "The process of porting Honey Soldier to the Genesis/Megadrive"
+    }
+}
