5aa0d4f25e69b4b5b7c0f175dabd05d9ceb67138
IoriBranford <ioribranford@gmail.com>
2026-07-20 19:04:49 -0700

    Add dragontail card

---
+++ pages/dragontail/props.ts
@@ -0,0 +1,11 @@
+import GameProps from "../GameProps";
+import image from './card.png?url'
+
+export const documentProps: GameProps = {
+    "id": "dragontail",
+    "title": "Rose of Dragontail",
+    "description": "Kick tail with tail! NEW in development!",
+    "primaryColor": "#b51b1b",
+    "secondaryColor": "#ff6b6b",
+    image
+}
