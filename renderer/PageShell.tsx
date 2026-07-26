f1aeffcd403b03ca8615c50399246d8819237cae
IoriBranford <ioribranford@gmail.com>
2026-07-26 08:22:26 -0700

    Add dragontail to the nav

--- renderer/PageShell.tsx
+++ renderer/PageShell.tsx
@@ -29,6 +29,7 @@ export function Header({ url }) {
 					<li><details role='list'>
 						<summary aria-haspopup="listbox" role="link">Games</summary>
 						<ul role="listbox">
+							<PageNav href='/dragontail' url={url}>Rose of Dragontail</PageNav>
 							<PageNav href='/demonizer' url={url}>Demonizer</PageNav>
 							<PageNav href='/honeysoldier' url={url}>Honey Soldier</PageNav>
 						</ul>
